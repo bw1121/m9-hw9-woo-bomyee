57ac399b5ca370d77dadd86fe387c25bfbde3de2
Bomyee Woo <bomyeew@gmail.com>
2020-07-24 10:44:45 -0400

    Customize plugin options

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-	$('#pagepiling').pagepiling();
+	$('#pagepiling').pagepiling({
+        sectionsColor: ['#BDCFF2', '#D0DFF2', '#D8EBF2', '#B6E7F2', '#BDF2D0'],
+        navigation: {
+            'textColor': '#fff',
+            'bulletsColor': '#fff',
+            'position': 'right',
+            'tooltips': ['Home', 'Portfolio Item 1', 'Portfolio Item 2', 'Portfolio Item 3', 'Learn More']
+        },
+        loopBottom: 'true'
+    });
 });
